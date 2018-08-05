beee70b6341b194c5780a5f49bd4d21e7357d2fd
ravinder <ravinder@titanicapp.co>
2018-08-05 18:25:48 +0530

    removed redundant code

--- app/v1/enterurl/EnterUrlResourceHandler.scala
+++ app/v1/enterurl/EnterUrlResourceHandler.scala
@@ -2,7 +2,6 @@ package v1.enterurl
 
 import java.util.{Date, UUID}
 
-import data_repository.UserRepositoryImpl
 import javax.inject.{Inject, Provider}
 import utils.{ApplicationResult, Config, Money}
 
@@ -13,6 +12,5 @@ import scala.concurrent.{ExecutionContext, Future}
   * Controls access to and creation of the backend data
   */
 class EnterUrlResourceHandler @Inject()(routerProvider: Provider[EnterUrlRouter],
-                                        userRepository: UserRepositoryImpl,
                                         config: Config)(implicit ec: ExecutionContext) {
 }
